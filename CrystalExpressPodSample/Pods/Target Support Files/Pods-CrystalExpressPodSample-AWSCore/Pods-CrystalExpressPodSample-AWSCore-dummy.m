eb7db5b325dde20e13943aef9fb3497b6fc34025
roylo <roylo@intowow.com>
2015-11-02 14:33:10 +0800

    update SDK to 1.5.0

--- CrystalExpressPodSample/Pods/Target Support Files/Pods-CrystalExpressPodSample-AWSCore/Pods-CrystalExpressPodSample-AWSCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CrystalExpressPodSample_AWSCore : NSObject
-@end
-@implementation PodsDummy_Pods_CrystalExpressPodSample_AWSCore
-@end
--- CrystalExpressPodSample/Pods/Target Support Files/Pods-CrystalExpressPodSample-AWSKinesis/Pods-CrystalExpressPodSample-AWSKinesis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CrystalExpressPodSample_AWSKinesis : NSObject
-@end
-@implementation PodsDummy_Pods_CrystalExpressPodSample_AWSKinesis
-@end
